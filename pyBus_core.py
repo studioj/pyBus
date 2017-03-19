2cc9792625cd06dfd3e161f8c60b4730ff8438b2
vonStauffenFeld <pierre.vivegnis@belgacom.net>
2017-03-19 01:54:03 +0100

    Small modifs

--- pyBus_core.py
+++ pyBus_core.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import os
 import sys
@@ -20,6 +21,7 @@ from pyBus_interface import *
 # GLOBALS
 #####################################
 DEVPATH           = "/dev/ttyUSB0" # This is a default, but its always overridden. So not really a default.
+LOGFILE           = "/tmp/pyBus.log" # The logfile. Other logs are compressed and archived. Use zcat to get their content.
 IBUS              = None
 REGISTERED        = False # This is a temporary measure until state driven behaviour is implemented
 
@@ -30,25 +32,25 @@ REGISTERED        = False # This is a temporary measure until state driven behav
 def initialize():
   global IBUS, REGISTERED, DEVPATH
   REGISTERED=False
-  
+
   # Initialize the iBus interface or wait for it to become available.
   while IBUS == None:
     if os.path.exists(DEVPATH):
       IBUS = ibusFace(DEVPATH)
     else:
-      logging.warning("USB interface not found at (%s). Waiting 1 seconds.", DEVPATH)
+      logging.warning("USB interface not found at (%s). Waiting 2 seconds.", DEVPATH)
       time.sleep(2)
   IBUS.waitClearBus() # Wait for the iBus to clear, then send some initialization signals
   pB_eDriver.init(IBUS)
-  
+
 # close the USB device and whatever else is required
 def shutdown():
   global IBUS
   logging.info("Shutting down event driver")
   pB_eDriver.shutDown()
-  
+
   if IBUS:
-    logging.info("Killing iBUS instance")
+    logging.info("Killing iBus instance")
     IBUS.close()
     IBUS = None
 
