8f90b2cdd9f0c16f92e2306e34b656890fa6910b
Jef Neefs <neefsj@gmail.com>
2018-08-13 23:02:09 +0200

    adding setup.py and travis yml

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""
+# ==============================================================================
+# Name:             setup.py
+#                   -
+# Purpose:          -
+#                   -
+#                   -
+# Author:           Jef Neefs
+# Created:          13/08/2018
+# Copyright:        (c) Jef Neefs 2018
+# Licence:          GPLv3
+# ==============================================================================
+# Extra used pypi modules which may need to be installed:
+#
+# ==============================================================================
+"""
+# !/usr/bin/env python
+
+from distutils.core import setup
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='pyBus',
+      version='0.0.01',
+      description='Python Distribution Utilities',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='studioj',
+      author_email='neefsj@mail.com',
+      url='https://github.com/studioj/pyBus',
+      packages=['serial', 'mpd'],
+      )
