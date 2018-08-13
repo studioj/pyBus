1dd78967917e319805f967ba1fa551b3290705c3
Jef Neefs <neefsj@gmail.com>
2018-08-13 23:49:55 +0200

    adding empty test suite

---
+++ tests/test_pybus.py
@@ -0,0 +1,20 @@
+"""
+# ==============================================================================
+# Name:             test_pybus
+#                   -
+# Purpose:          -
+#                   -
+#                   -
+# Author:           Jef Neefs
+# Created:          13/08/2018
+# Copyright:        (c) Jef Neefs 2018
+# Licence:          GPLv3
+# ==============================================================================
+# Extra used pypi modules which may need to be installed:
+#
+# ==============================================================================
+"""
+
+
+def test_something():
+    assert True
